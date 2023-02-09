8682f583434143b26b8add47308a06cb720ed90c
mustafe <{duurxul@gmail.com}+{mustafe}@users.noreply.github.com>
2023-02-09 20:24:30 +0100

    Latest initial

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,18 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  this.scrollY >= 50
+    ? header.classList.add("scroll-header")
+    : header.classList.remove("scroll-header");
+};
+window.addEventListener("scroll", scrollHeader);
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-
